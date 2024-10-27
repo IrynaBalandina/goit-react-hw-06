5be1beac2cb5656b80b6974c8156273270ee48f0
Iryna <mefodievna2908@gmail.com>
2024-10-27 17:39:46 +0200

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,22 @@
+import { configureStore } from "@reduxjs/toolkit";
 
+ const initialState = {
+    
+        contacts: 
+            JSON.parse(localStorage.getItem("contacts")) || dataContacts
+        ,
+    
+        
+    
+    filters: {
+      status: "all",
+    },
+  };
+
+const rootReducer = (state=initialState, action) => {
+    return state;
+  };
   
-  
+  export const store = configureStore({
+    reducer: rootReducer,
+  });
